3de05be0ac52f5880a4ac300e32db9beb5657e21
Will <20273409+novucs@users.noreply.github.com>
2025-09-02 14:47:52 +0100

    release 0.2.1

--- examples/testcontainers_example.py
+++ examples/testcontainers_example.py
@@ -1,7 +1,7 @@
 from google.cloud import bigquery
 from testcontainers.core.container import DockerContainer
 
-bigquery_image = "ghcr.io/novucs/local-bigquery:0.2.0"
+bigquery_image = "ghcr.io/novucs/local-bigquery:0.2.1"
 with DockerContainer(bigquery_image).with_exposed_ports(9050) as container:
     host = container.get_container_host_ip()
     port = container.get_exposed_port(9050)
